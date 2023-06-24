0ec662f292e09c484477c939e48007e55c73277c
Allan-Lande <137254648+Allan-Lande@users.noreply.github.com>
2023-06-24 13:30:47 +0300

    added ofcourse

--- hungry.py
+++ hungry.py
@@ -1,5 +1,5 @@
 hungry = input("Are you hungry?")
-if hungry == "yes" and hungry == "Y":
+if hungry == "yes" and hungry == "ofcourse":
     print("Eat something")
     print("Drink something")
     print("Have some rest")
